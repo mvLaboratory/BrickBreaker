234d2ad44ad406215ac97d9fe9ac08c3ac65bfd9
mvLaboratory <mvLabaratory@gmail.com>
2015-04-06 22:54:59 +0300

    0.05

--- core/src/com/MVlab/BrickBreaker/gameworld/GameRenderer.java
+++ core/src/com/MVlab/BrickBreaker/gameworld/GameRenderer.java
@@ -107,7 +107,8 @@ public class GameRenderer  implements Disposable {
     }
 
     public void resize(int width, int height) {
-
+        cam.viewportWidth = (Consts.VIEWPORT_HEIGHT / height) * width;
+        cam.update();
     }
 
     @Override

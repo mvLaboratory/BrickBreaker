2c6683e21cab9d9e431c7f460c1d1859f21d2441
mvLaboratory <mvLabaratory@gmail.com>
2015-04-19 12:28:14 +0300

    0.08

--- core/src/com/MVlab/BrickBreaker/utils/Consts.java
+++ core/src/com/MVlab/BrickBreaker/utils/Consts.java
@@ -5,14 +5,14 @@ package com.MVlab.BrickBreaker.utils;
  */
 public class Consts {
     //game consts
-    public static float VIEWPORT_WIDTH = 15;
-    public static float VIEWPORT_HEIGHT = 30;
-    public static float GAME_LEFT_BORDER = -7.4f;
-    public static float GAME_RIGHT_BORDER = 4f;
-    public static float GAME_TOP_BORDER = 13f;
-    public static float GAME_BOTTOM_BORDER = -13f;
+    public static float VIEWPORT_WIDTH = 10;
+    public static float VIEWPORT_HEIGHT = 15;
+    public static float GAME_LEFT_BORDER = -4.9f;
+    public static float GAME_RIGHT_BORDER = 3f;
+    public static float GAME_TOP_BORDER = 6.5f;
+    public static float GAME_BOTTOM_BORDER = -6.7f;
 
-    public static float BRICKS_PER_ROW = 8f;
+    public static float BRICKS_PER_ROW = 5f;
 
     public static final String BASIC_TEXTURES_ATLAS_OBJECT = "data/textures/BrickBreackerTextures.pack";
 }

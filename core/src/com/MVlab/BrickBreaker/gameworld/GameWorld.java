191c67c93c483161568687ee775cf6a1cb87aa84
mvLaboratory <mvLabaratory@gmail.com>
2015-05-12 00:16:33 +0300

    0.1.0.6

--- core/src/com/MVlab/BrickBreaker/gameworld/GameWorld.java
+++ core/src/com/MVlab/BrickBreaker/gameworld/GameWorld.java
@@ -2,7 +2,7 @@
  * Created by MV on 17.03.2015.
  */
 
-package com.MVlab.BrickBreaker.gameWorld;
+package com.MVlab.BrickBreaker.gameworld;
 import com.MVlab.BrickBreaker.gameObjects.Border;
 import com.MVlab.BrickBreaker.gameObjects.BottomBorder;
 import com.MVlab.BrickBreaker.gameObjects.Brick;
@@ -11,6 +11,8 @@ import com.MVlab.BrickBreaker.gameObjects.RightBorder;
 import com.MVlab.BrickBreaker.utils.Consts;
 import com.MVlab.BrickBreaker.gameObjects.Ball;
 import com.MVlab.BrickBreaker.gameObjects.Racket;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Contact;
@@ -22,6 +24,7 @@ import com.badlogic.gdx.physics.box2d.World;
 import java.util.ArrayList;
 
 public class GameWorld  implements ContactListener {
+    public static final String TAG = GameWorld.class.getName();
     private Racket racket;
     private Ball ball;
     private LeftBorder leftBorder;
@@ -30,36 +33,64 @@ public class GameWorld  implements ContactListener {
     private BottomBorder bottomBorder;
     private World physicWorld;
     ArrayList<Brick> bricks;
-    private float screenWidth;
-    private float screenHeight;
-    private Boolean stoped;
-    private Boolean restart;
     private int score;
+    private int extraLivesCount;
+    private int levelNumber;
     private float gameDuration;
+    private float dropDuration;
+    private gameState presentGameState;
 
-    public GameWorld(float screenWidth, float screenHeight) {
-        this.screenHeight = screenHeight;
-        this.screenWidth = screenWidth;
+    public enum gameState {start, restart, levelStart, active, paused, dropped, gameOver};
+
+    public GameWorld() {
+        extraLivesCount = Consts.EXTRA_LIFE_CONT;
+        bricks = new ArrayList<Brick>();
+
+        presentGameState = gameState.start;
+
         init();
     }
 
     public void init() {
-        bricks = new ArrayList<Brick>();
-
         physicWorld = new World(new Vector2(0, -3F), true);
         physicWorld.setContactListener(this);
 
         racket = new Racket(Consts.GAME_CENTER, -2.3f, 1f, 0.2f, 3f, physicWorld);
-        ball = new Ball(Consts.GAME_CENTER, -1, 0.3f, physicWorld);
-
-        stoped = false;
-        restart = false;
+        ball = new Ball(Consts.GAME_CENTER, -2.15f, 0.3f, physicWorld);
 
         leftBorder = new LeftBorder(Consts.GAME_LEFT_BORDER + 0.15f, -0.5f, 0.15f, Consts.GAME_TOP_BORDER, physicWorld);
         rightBorder = new RightBorder(Consts.GAME_RIGHT_BORDER, -0.5f, 0.15f, Consts.GAME_TOP_BORDER, physicWorld);
         topBorder = new Border(Consts.GAME_RIGHT_BORDER - ((Consts.GAME_RIGHT_BORDER - Consts.GAME_LEFT_BORDER) / 2) - 0.01f, Consts.GAME_TOP_BORDER - 0.5f, (Consts.GAME_RIGHT_BORDER - Consts.GAME_LEFT_BORDER) / 2, 0.15f, physicWorld);
         bottomBorder = new BottomBorder(Consts.GAME_RIGHT_BORDER - ((Consts.GAME_RIGHT_BORDER - Consts.GAME_LEFT_BORDER) / 2) - 0.01f, Consts.GAME_BOTTOM_BORDER - 0.5f, (Consts.GAME_RIGHT_BORDER - Consts.GAME_LEFT_BORDER) / 2, 0.01f, physicWorld);
 
+        if (presentGameState == gameState.start){
+            score = 0;
+            gameDuration = 0;
+            levelNumber = 0;
+        }
+        dropDuration = 0;
+
+        initLevel();
+    }
+
+    public void initLevel() {
+        ArrayList<Brick> tempBricks = new ArrayList<Brick>();
+
+        if (levelStart()) {
+            loadLevel();
+        }
+        else {
+            for (Brick brick : bricks) {
+                if (! brick.existing()) continue;
+                tempBricks.add(new Brick(brick.getBrickX(), brick.getBrickY(), brick.getBrickWidth(), brick.getBrickHeight(), physicWorld));
+            }
+            bricks.clear();
+            bricks.addAll(tempBricks);
+        }
+    }
+
+    public void loadLevel() {
+        bricks.clear();
         Vector2 brickPosition = new Vector2(-3.5f, 2.5f);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < Consts.BRICKS_PER_ROW; j++) {
@@ -68,22 +99,72 @@ public class GameWorld  implements ContactListener {
             }
             brickPosition.x = -3.5f;
         }
-
-        score = 0;
-        gameDuration = 0;
+        levelNumber++;
     }
 
     public void update(float delta) {
-        physicWorld.step(delta, 1, 1);
-        gameDuration += delta;
+        if (active()) {
+            physicWorld.step(delta, 1, 1);
+            gameDuration += delta;
 
-        for (Brick brick : bricks) {
-            if (brick != null) {
-                brick.update();
+            int activeBricksCount = 0;
+            for (Brick brick : bricks) {
+                if (brick != null) {
+                    brick.update();
+                    if (brick.existing()) activeBricksCount++;
+                }
+            }
+            if (activeBricksCount == 0) {
+                presentGameState = gameState.levelStart;
+                initLevel();
+            }
+
+            racket.update(delta);
+            ball.update();
+            if (ball.underBottomBorder()) {
+                drop();
             }
         }
-        racket.update(delta);
-        ball.update();
+
+        if (presentGameState == gameState.dropped) {
+            if (dropDuration > Consts.DROP_DURATION) {
+                presentGameState = gameState.restart;
+                dropDuration = 0;
+            }
+            dropDuration += delta;
+        }
+    }
+
+    public void kickTheBall() {
+        int ballDirectionX = MathUtils.random(-1, 2);
+        ballDirectionX = ballDirectionX > 0 ? 1 : -1;
+        ball.getPhysicBody().applyLinearImpulse(15 * ballDirectionX, 80, 0, 0, true);
+        presentGameState = gameState.active;
+    }
+
+    public void scored() {
+        score += 1;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void drop() {
+        extraLivesCount--;
+        presentGameState = gameState.dropped;
+    }
+
+    public Boolean active() {
+        return presentGameState == gameState.active;
+    }
+
+    public Boolean needRestart() {
+        return presentGameState == gameState.restart || presentGameState == gameState.levelStart;
+    }
+
+    public void restart() {
+        presentGameState = gameState.restart;
     }
 
     public Racket getRacket() {
@@ -112,6 +193,34 @@ public class GameWorld  implements ContactListener {
 
     public ArrayList<Brick> getBricks() {
         return bricks;
+    }
+
+    public float getGameDuration() {
+        return gameDuration;
+    }
+
+    public int getExtraLivesCount() {
+        return extraLivesCount;
+    }
+
+    public void rebootExtraLivesCount() {
+        extraLivesCount = Consts.EXTRA_LIFE_CONT;
+    }
+
+    public boolean levelStart() {
+        return presentGameState == gameState.levelStart || presentGameState == gameState.start;
+    }
+
+    public gameState getPresentGameState() {
+        return  presentGameState;
+    }
+
+    public void setPresentGameState(gameState presentGameState) {
+        this.presentGameState = presentGameState;
+    }
+
+    public int getLevelNumber() {
+        return levelNumber;
     }
 
     @Override
@@ -164,35 +273,8 @@ public class GameWorld  implements ContactListener {
 
         //Bottom border
         if ((bodyA instanceof Ball && bodyB instanceof BottomBorder) || (bodyA instanceof BottomBorder && bodyB instanceof Ball)) {
-            stoped = true;
+            drop();
         }
     }
 
-    public void scored() {
-        score += 1;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void stop() {
-        stoped = true;
-    }
-
-    public Boolean active() {
-        return !stoped;
-    }
-
-    public Boolean needRestart() {
-        return restart;
-    }
-
-    public void restart() {
-        restart = true;
-    }
-
-    public float getGameDuration() {
-        return gameDuration;
-    }
 }

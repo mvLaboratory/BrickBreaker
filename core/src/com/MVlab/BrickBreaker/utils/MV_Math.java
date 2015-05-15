e22a6a6b89a31b60cf293ee0184a6aa63e35e945
mvLaboratory <mvLabaratory@gmail.com>
2015-05-15 22:24:25 +0300

    0.1.1.0

--- core/src/com/MVlab/BrickBreaker/utils/MV_Math.java
+++ core/src/com/MVlab/BrickBreaker/utils/MV_Math.java
@@ -7,4 +7,20 @@ public class MV_Math {
     public static float abs(float x) {
         return x < 0 ? x * -1 : x;
     }
+
+    public static float max(float x, float y) {
+        return x < y ? y : x;
+    }
+
+    public static int max(int x, int y) {
+        return x < y ? y : x;
+    }
+
+    public static float min(float x, float y) {
+        return x > y ? y : x;
+    }
+
+    public static int min(int x, int y) {
+        return x > y ? y : x;
+    }
 }

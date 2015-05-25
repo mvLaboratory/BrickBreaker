47d8c859a1edb234980a71637c3ac83581a96e4a
mvLaboratory <mvLabaratory@gmail.com>
2015-05-25 22:12:12 +0300

    0.1.1.0

--- core/src/com/MVlab/BrickBreaker/screens/AbstractGameScreen.java
+++ core/src/com/MVlab/BrickBreaker/screens/AbstractGameScreen.java
@@ -1,7 +1,52 @@
 package com.MVlab.BrickBreaker.screens;
 
+import com.MVlab.BrickBreaker.Assets;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+
 /**
  * Created by MV on 18.05.2015.
  */
-public class AbstractGameScreen {
+public abstract class AbstractGameScreen implements Screen {
+    protected Game game;
+
+    protected AbstractGameScreen(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+        Assets.instance.init(new AssetManager());
+    }
+
+    @Override
+    public void dispose() {
+        Assets.instance.dispose();
+    }
 }

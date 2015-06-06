208a9976e5ab3f47ebdfb00b5f3dbbcf5acfff7d
mvLaboratory <mvLabaratory@gmail.com>
2015-06-06 23:57:00 +0300

    0.1.1.5

--- desktop/src/com/MVlab/BrickBreaker/desktop/DesktopLauncher.java
+++ desktop/src/com/MVlab/BrickBreaker/desktop/DesktopLauncher.java
@@ -7,11 +7,11 @@ import com.MVlab.BrickBreaker.BrickBreakerCore;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "Brick breaker";
+        config.title = "Stuff breaker";
 //        config.width = 272;
 //        config.height = 408;
         config.width = 300;
-        config.height = 500;
+        config.height = 450;
 		new LwjglApplication(new BrickBreakerCore(), config);
 	}
 }
